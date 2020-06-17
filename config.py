9a05c5cb7df349be04de2d94adc061a9d09cc4f5
Sean Ziegler <sziegler0@gmail.com>
2020-06-16 22:00:36 -0400

    Formatted files and improved README

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 import os
 
+
 class DevConfig():
     DEBUG = True
     CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://sziegler:devpassword@gatorlfpostgres.cfk7jbe8i73l.us-east-1.rds.amazonaws.com:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = ""
     FLASK_ENV = "development"
     SECRET_KEY = os.urandom(32)
 
+
 class TestConfig():
     pass
+
 
 class ProdConfig():
     pass
--- manage.py
+++ manage.py
@@ -4,10 +4,5 @@ import os
 from app import app, db
 
 
-
-
-
-
-
 if __name__ == '__main__':
     manager.run()
